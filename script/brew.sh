014dab3d6aad672aad35db83bbc4a9e4e0427c9b
enomoto <kenta.enomoto@classi.jp>
2019-07-18 15:36:27 +0900

    Add install.sh

---
+++ script/brew.sh
@@ -0,0 +1,55 @@
+# Update Homebrew, formulae, and packages
+
+brew update
+brew upgrade
+apps=(
+  git
+  git-lfs
+  tig
+  ghq
+  tree
+  hub
+  peco
+  lua
+  vim
+  ctags
+  wget
+  awscli
+  mysql
+  rbenv
+  ruby-build
+  redis
+  pyenv
+  pyenv-virtualenv
+  python3
+  ffmpeg
+  cmake
+  ninja
+  swiftlint
+  sbt
+  golang
+  carthage
+  cloc
+)
+
+brew install "${apps[@]}"
+
+apps=(
+  abstract
+  android-studio
+  charles
+  docker
+  figma
+  flux
+  google-chrome
+  intellij-idea-ce
+  iterm2
+  jasper
+  macdown
+  pusher
+  skitch
+  slack
+)
+
+brew cask install "${apps[@]}"
+
